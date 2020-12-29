2adc625bfdf870ffde3e5fcac401c5f024479069
Nazmul Alam <nazmuldipu@gmail.com>
2020-12-29 09:24:34 +0600

    dashboard component added

--- src/dashboard/containers/index/index.component.ts
+++ src/dashboard/containers/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/service/auth.service';
 
 @Component({
   selector: 'app-index',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  user;
+  quickLinks: QuickLinks[];
 
-  constructor() { }
+  constructor(public auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.quickLinks = [
+      { name: 'Loading Registration', url: '/dashboard/add-loading' },
+      { name: 'Loading List', url: '/dashboard/loading' },
+    ];
+
+    this.auth.user$.subscribe(data => {
+      if (data) {
+        this.user = { _id: data.uid, email: data.email };
+      } else {
+        this.user = null;
+      }
+    })
   }
 
+  isAuthenticated() {
+    return !!this.user;
+  }
+
+  onLinkClick(event) {
+    console.log(event.value);
+    this.router.navigate([event.value.url]);
+  }
+}
+
+interface QuickLinks {
+  name: string,
+  url: string
 }
--- src/dashboard/containers/loading/loading.component.ts
+++ src/dashboard/containers/loading/loading.component.ts
@@ -36,7 +36,6 @@ export class LoadingComponent implements OnInit {
   async getLoadingList() {
     this.loadingService.loadings$.subscribe((data) => {
       this.loadingList = data;
-      console.log(this.loadingList);
       this.loadingList.sort(this.util.dynamicSortObject('priority'));
       this.refreshLoading();
     });
